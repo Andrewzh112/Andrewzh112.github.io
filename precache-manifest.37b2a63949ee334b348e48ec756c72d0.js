4a33e9911a9a8de0c177d39958079241144fd1c0
Andrew Zhao <andrewzhao112@gmail.com>
2025-10-21 10:29:58 +0800

    Updates

--- precache-manifest.37b2a63949ee334b348e48ec756c72d0.js
+++ precache-manifest.37b2a63949ee334b348e48ec756c72d0.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bb09640274bd1d5e78527c3648c1e253",
+    "revision": "b0093dc42f20166278dd0a4780c5f5e5",
     "url": "/index.html"
   },
   {
-    "revision": "2d2b2fe88103e5602005",
-    "url": "/static/css/main.5c75e3e8.chunk.css"
+    "revision": "8755f018742c264dfb4a",
+    "url": "/static/css/main.7f8695b5.chunk.css"
   },
   {
-    "revision": "dde58577c343a5a074df",
-    "url": "/static/js/2.3fc4b11e.chunk.js"
+    "revision": "752afc60fa263df8f970",
+    "url": "/static/js/2.7692fcca.chunk.js"
   },
   {
     "revision": "c309a0a8222cb38a7f72b62ee15e4219",
-    "url": "/static/js/2.3fc4b11e.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.7692fcca.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2d2b2fe88103e5602005",
-    "url": "/static/js/main.d6f55e16.chunk.js"
+    "revision": "8755f018742c264dfb4a",
+    "url": "/static/js/main.3c6cad7b.chunk.js"
   },
   {
     "revision": "430f72daf7c3a4d42642",
@@ -26,10 +26,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "2d63a409efcc0ce058d82a4507020de2",
     "url": "/static/media/azr.2d63a409.png"
-  },
-  {
-    "revision": "313cfd66761c9f0f9215671ac0d182a8",
-    "url": "/static/media/beyond.313cfd66.png"
   },
   {
     "revision": "d499079a37e5ef7f02111f3a63cff1fb",
@@ -52,6 +48,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/focus2.f32b3428.png"
   },
   {
+    "revision": "5327e109105de7df9371cf0965c40600",
+    "url": "/static/media/forking.5327e109.png"
+  },
+  {
     "revision": "ae15560282645d32dc92a2bc03e7abad",
     "url": "/static/media/gfucb.ae155602.png"
   },
@@ -70,6 +70,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "ccae196bff3ad509ebd36528622de020",
     "url": "/static/media/optim.ccae196b.png"
+  },
+  {
+    "revision": "16cf87d0d0acb2bb3a594a3bd837958c",
+    "url": "/static/media/prompt.16cf87d0.png"
   },
   {
     "revision": "256f97bfd23a2c93ec7b3c579e02aedb",
